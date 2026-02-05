c7da13b68bc1496d4356006f45ef3b904a0f3238
Kartikye Saini <kartikyesaini2@gmail.com>
2026-02-05 14:42:06 +0530

    344 leetcode

---
+++ leetcode344.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+       int st = 0;
+       int end = s.size()-1;
+        while(st<end){
+            char temp = s[st];
+            s[st]= s[end];
+            s[end] = temp;
+            st++;
+            end--;
+        }
+        // for(char c :s){
+        //     cout<<c;
+        // }
+    }
+};
