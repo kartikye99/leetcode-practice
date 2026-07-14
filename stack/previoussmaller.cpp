ca4905bfa928a7c6158ceb97dc202283f3d07490
Kartikye Saini <kartikyesaini2@gmail.com>
2026-07-14 11:28:14 +0530

    implemented previous  smaller element

---
+++ stack/previoussmaller.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    vector<int> prevSmaller(vector<int>& arr) {
+        //  code here
+        stack<int>st;
+        int n = arr.size();
+        vector<int>ans(n);
+        for(int i = 0 ; i<arr.size();i++){
+            
+            while(!st.empty() && st.top() >= arr[i]){
+                st.pop();
+            }
+             if(st.empty()){
+                ans[i] = -1;
+            }
+            else{
+                ans[i] = st.top();
+            }
+            st.push(arr[i]);
+        }
+        return ans;
+
+    }
+};
